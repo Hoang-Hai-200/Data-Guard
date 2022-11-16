3c5048ce5ea8a893130f5568f97d7eba7ae43002
Hoang-Hai-200 <64273754+Hoang-Hai-200@users.noreply.github.com>
2022-11-16 04:12:06 -0800

    Update primary_setup2.sh

--- primary_setup2.sh
+++ primary_setup2.sh
@@ -11,6 +11,10 @@ alter database force logging;
 alter system set standby_file_management=auto;
 alter database open;
 create pfile='/home/oracle/pfile_primary.ora' from spfile;
+alter database add standby logfile thread 1 group 10 ('/u01/app/oracle/oradata/DB1/standby_redo10.log') size 200m;
+alter database add standby logfile thread 1 group 11 ('/u01/app/oracle/oradata/DB1/standby_redo11.log') size 200m;
+alter database add standby logfile thread 1 group 12 ('/u01/app/oracle/oradata/DB1/standby_redo12.log') size 200m;
+alter database add standby logfile thread 1 group 13 ('/u01/app/oracle/oradata/DB1/standby_redo13.log') size 200m;
 EOF
 
 
