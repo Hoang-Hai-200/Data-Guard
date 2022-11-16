1c2e4ad9f62cf7f4908b04dae213656d359172a6
Hoang-Hai-200 <64273754+Hoang-Hai-200@users.noreply.github.com>
2022-11-16 03:27:50 -0800

    standby_setup

---
+++ standby_setup_dataguard.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cat > /u01/app/oracle/product/19.0.0/dbhome_1/network/admin/tnsnames.ora << EOFTNS
+LISTENER =
+  (ADDRESS = (PROTOCOL = TCP)(HOST = standby)(PORT = 1521))
+
+standbytns  =
+  (DESCRIPTION =
+    (ADDRESS = (PROTOCOL = TCP)(HOST = standbyos)(PORT = 1521))
+    (CONNECT_DATA =
+      (SERVER = DEDICATED)
+      (SERVICE_NAME = db1)
+    )
+  )
+
+
+primarytns =
+  (DESCRIPTION =
+    (ADDRESS = (PROTOCOL = TCP)(HOST = primaryos)(PORT = 1521))
+    (CONNECT_DATA =
+      (SERVER = DEDICATED)
+      (SERVICE_NAME = db1)
+    )
+  )
+EOFTNS
+
+
+cat > /u01/app/oracle/product/19.0.0/dbhome_1/network/admin/listener.ora << EOFLIS
+
+LISTENER =
+  (DESCRIPTION_LIST =
+    (DESCRIPTION =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = standbyos)(PORT = 1521))
+      (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1521))
+    )
+  )
+
+SID_LIST_LISTENER =
+  (SID_LIST =
+    (SID_DESC =
+      (ORACLE_HOME = /u01/app/oracle/product/19.0.0/dbhome_1)
+      (SID_NAME = db1)
+      (ENVS="TNS_ADMIN=/u01/app/oracle/product/19.0.0/dbhome_1/network/admin")
+    )
+  )
+
+ADR_BASE_LISTENER = /u01/app/oracle
+EOFLIS
