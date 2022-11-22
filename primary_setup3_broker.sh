1d9812ed2d83abcfb69a4c2ebf13b28379dcc762
Hoang-Hai-200 <64273754+Hoang-Hai-200@users.noreply.github.com>
2022-11-22 11:16:06 +0700

    Update primary_setup3_broker.sh

--- primary_setup3_broker.sh
+++ primary_setup3_broker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 dgmgrl sys/admin@primarytns << EOFDG
-alter system set dg_broker_start=true;
 create configuration my_dg_config as primary database is uprimary connect identifier is primarytns;
 add database ustandby as connect identifier is standbytns maintained as physical;
 enable configuration;
